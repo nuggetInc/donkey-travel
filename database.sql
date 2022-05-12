6a2c6110dca824effbb7fe3bc180c01e9680eb17
Nugget <cdl.0404@gmail.com>
2022-05-12 12:05:00 +0200

    login fix, new db file

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 10, 2022 at 12:39 PM
+-- Generation Time: May 12, 2022 at 12:04 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -48,6 +48,15 @@ CREATE TABLE `customers` (
   `password` varchar(100) NOT NULL,
   `edited` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`id`, `name`, `email`, `phonenumber`, `password`, `edited`) VALUES
+(1, 'Cas', 'cas@donkeytravel.nl', '0612345678', '$2y$10$H4rIShA0C00E5EvTEusgLu5JTs1/TNyOkskTbz8VV567FEArh4b12', '2022-05-12 10:03:37'),
+(2, 'Tim', 'tim@donkeytravel.nl', '0612345678', '$2y$10$99pqDMy8/7ivZX50261Y7e3PCwV9ejVKNvg0Ywyqv.nPkWW9qbwi.', '2022-05-12 10:03:41'),
+(3, 'Daan', 'daan@donkeytravel.nl', '0612345678', '$2y$10$pmaAOJeSk0J7aOX6KZy3o.1twnTT6vbBiGxl3nHRQH/29zgPOzGZe', '2022-05-11 07:17:06');
 
 -- --------------------------------------------------------
 
@@ -231,7 +240,7 @@ ALTER TABLE `breakspots`
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `inns`
--- pages/inloggen.php
+++ pages/inloggen.php
@@ -14,8 +14,8 @@ if (isset($_POST["login"]))
         {
             $_SESSION["user"] = $customer;
 
-            header("Location: " . ROOT_DIR);
-            exit;
+            // header("Location: " . ROOT_DIR);
+            // exit;
         }
     }
 
@@ -24,9 +24,12 @@ if (isset($_POST["login"]))
     $_SESSION["login-email"] = $email;
     $_SESSION["login-password"] = $password;
 
-    header("Location: " . ROOT_DIR . "inloggen");
-    exit;
+    // header("Location: " . ROOT_DIR);
+    // exit;
 }
+
+
+echo password_hash($_SESSION["login-password"], PASSWORD_DEFAULT);
 
 $email = htmlspecialchars($_SESSION["login-email"] ?? "");
 $password = htmlspecialchars($_SESSION["login-password"] ?? "");
