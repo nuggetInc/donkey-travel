c5ae596e87e4160df76e6b3dd85b0294e9ae9392
Cas de Loijer <cdl.0404@gmail.com>
2022-05-24 09:51:31 +0200

    error for account edit

--- pages/account/wijzigen.php
+++ pages/account/wijzigen.php
@@ -6,28 +6,56 @@ $customer = Customer::get($_SESSION["customerID"]);
 
 if (isset($_POST["edit"]))
 {
+    $customerID = $_SESSION["customerID"];
+    $name = $_POST["name"];
+    $email =  $_POST["email"];
+    $phonenumber = $_POST["phonenumber"];
+
     if ($_POST["password"] === "")
         $password = $customer->getPassword();
     else
         $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
-    Customer::update(
-        $_SESSION["customerID"],
-        $_POST["name"],
-        $_POST["email"],
-        $_POST["phonenumber"],
-        $password
-    );
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+    {
+        $_SESSION["error"] = "Het E-mailadres ongeldig";
 
-    mail($_POST["email"], "Account veranderingen", "Je account op donkeytravel.nl is successvol aangepast.");
+        $_SESSION["name"] = $name;
+        $_SESSION["email"] = $email;
+        $_SESSION["phonenumber"] = $phonenumber;
+
+        header("Location: " . ROOT_DIR . "account");
+        exit;
+    }
+
+    if (!filter_var($phonenumber, FILTER_SANITIZE_NUMBER_INT))
+    {
+        $_SESSION["error"] = "Het telefoonnummer is ongeldig";
+
+        $_SESSION["name"] = $name;
+        $_SESSION["email"] = $email;
+        $_SESSION["phonenumber"] = $phonenumber;
+
+        header("Location: " . ROOT_DIR . "account");
+        exit;
+    }
+
+    Customer::update($customerID, $name, $email, $phonenumber, $password);
+
+    mail($email, "Account veranderingen", "Je account op donkeytravel.nl is successvol aangepast.");
 
     header("Location: " . ROOT_DIR . "account");
     exit;
 }
 
 ?>
-<h2>Account wijzigen</h2>
 <form method="POST">
+    <h2>Account wijzigen</h2>
+
+    <?php if (isset($_SESSION["error"])) : ?>
+        <p><?= $_SESSION["error"] ?></p>
+    <?php endif ?>
+
     <label>Naam:
         <input type="text" name="name" value="<?= htmlspecialchars($customer->getName()) ?>" onfocus="this.select()" required />
     </label>
