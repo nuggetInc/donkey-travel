7c20bbad628dde29c2e69ec1a837d066dd523855
Cas de Loijer <cdl.0404@gmail.com>
2022-06-20 18:06:48 +0200

    create reservation

--- pages/boekingen/aanvragen.php
+++ pages/boekingen/aanvragen.php
@@ -1,23 +1,33 @@
-
 <?php
+
+declare(strict_types=1);
+
 $trips = Trip::getAll();
+
 ?>
-<form method="POST">
-    <label>Startdatum:</label>
-    <input type="date" name="date">
-    <label>Tocht:</label>
-    <select name="tripID">
-        <?php foreach ($trips as $id => $trip) : ?>
-            <option value="<?= $id ?>"><?= htmlspecialchars($trip->getRoute()) ?></option>
-        <?php endforeach ?>
-    </select>
-    <input type="submit" value="Aanvragen">
-</form>
+<div class="page-wrapper">
+    <form class="form" method="POST">
+        <header>Boeking aanvragen</header>
+        <label>
+            <header>Startdatum:</header>
+            <input type="date" name="date">
+        </label>
+        <label>
+            <header>Tocht:</header>
+            <select name="tripID">
+                <?php foreach ($trips as $id => $trip) : ?>
+                    <option value="<?= $id ?>"><?= htmlspecialchars($trip->getRoute()) ?></option>
+                <?php endforeach ?>
+            </select>
+        </label>
+        <input type="submit" value="Aanvragen">
+    </form>
+</div>
 <?php
 if (isset($_POST['date']) && isset($_POST['tripID'])) {
     // statusID is een probleem voor later :upsidedown:
     // pincode hoort ook geen 0 te zijn, maar dat lossen we later ook wel op :upsidedown:
     Reservation::create(strtotime($_POST['date']), 0, (int)$_POST['tripID'], $_SESSION['customerID'], 0);
-    header('Location: '.ROOT_DIR.'boekingen');
+    header('Location: ' . ROOT_DIR . 'boekingen');
 }
 ?>
