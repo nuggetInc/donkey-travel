09e0976c059cd875e45d1dc9e4b0def6db258b63
Cas de Loijer <cdl.0404@gmail.com>
2022-06-22 09:52:05 +0200

    added extra info to view reservation

--- pages/management/boekingen/bekijken.php
+++ pages/management/boekingen/bekijken.php
@@ -9,15 +9,35 @@ declare(strict_types=1);
 
         <label>
             <header>Startdatum:</header>
-            <input type="date" name="date" value="<?= date("Y-m-d", $reservation->getStartDate()) ?>" disabled />
+            <input type="date" value="<?= date("Y-m-d", $reservation->getStartDate()) ?>" disabled />
+        </label>
+
+        <label>
+            <header>Status:</header>
+            <select disabled>
+                <option><?= htmlspecialchars($reservation->getStatus()->getStatus()) ?></option>
+            </select>
+        </label>
+
+        <label>
+            <header>PIN code:</header>
+            <input type="text" value="<?= $reservation->getPincode() ?>" disabled />
         </label>
 
         <label>
             <header>Tocht:</header>
-            <select name="tripID" disabled>
+            <select disabled>
                 <option><?= htmlspecialchars($reservation->getTrip()->getRoute()) ?></option>
             </select>
         </label>
+
+        <label>
+            <header>Klant:</header>
+            <select disabled>
+                <option><?= htmlspecialchars($reservation->getCustomer()->getName()) ?></option>
+            </select>
+        </label>
+
 
         <footer>
             <a class="link" title="Wijzig deze boeking" href="<?= ROOT_DIR ?>management/boekingen/wijzigen?id=<?= $_GET["id"] ?>">Wijzigen</a>
