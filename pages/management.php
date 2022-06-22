d00f2c405ffa2151f058a949f54e115bd5bd164e
Cas de Loijer <cdl.0404@gmail.com>
2022-06-22 14:15:54 +0200

    added code for future management pages

--- pages/management.php
+++ pages/management.php
@@ -7,13 +7,22 @@ declare(strict_types=1);
     <a class="logo" href="<?= ROOT_DIR ?>management">Donkey<span class="green">Manager</span></a>
     <ul class="nav">
         <li><a class="link" title="Beheer boekingen" href="<?= ROOT_DIR ?>management/boekingen">Boekingen</a></li>
-        <li><a class="link" title="Beheer gasten" href="<?= ROOT_DIR ?>management/beheer">Beheer</a></li>
+        <li><a class="link" title="Beheer gasten" href="<?= ROOT_DIR ?>management/gasten">Gasten</a></li>
+        <li><a class="link" title="Beheer herbergen" href="<?= ROOT_DIR ?>management/herbergen">Herbergen</a></li>
+        <li><a class="link" title="Beheer restaurants" href="<?= ROOT_DIR ?>management/restaurants">Restaurants</a></li>
+        <li><a class="link" title="Beheer tochten" href="<?= ROOT_DIR ?>management/tochten">Tochten</a></li>
+        <li><a class="link" title="Beheer statussen" href="<?= ROOT_DIR ?>management/gasten">Statussen</a></li>
     </ul>
 </header>
 <?php
 
 switch ($path[1] ?? null) {
     case "boekingen":
+    case "gasten":
+    case "herbergen":
+    case "restaurants":
+    case "tochten":
+    case "gasten":
         require("pages/management/{$path[1]}.php");
         break;
     default:
